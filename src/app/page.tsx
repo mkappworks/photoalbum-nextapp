16e12257b40923d69c00453d45c12c843cdd6ed9
Malith Kuruppu <info.mkappworks@gmail.com>
2024-05-08 16:26:49 +0200

    Added Testimonials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { Hero } from "./_components/Hero";
+import { Testimonials } from "./_components/Testimonials";
 
 export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-white text-white dark:border-b-slate-700 dark:bg-background">
       <Hero />
+      <Testimonials />
     </main>
   );
 }
