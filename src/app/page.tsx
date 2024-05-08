2404ec8134f78276018ffea72eb51f6c70f4691b
Malith Kuruppu <info.mkappworks@gmail.com>
2024-05-08 16:41:13 +0200

    Added Features

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Features } from "./_components/Features";
 import { Hero } from "./_components/Hero";
 import { Pricing } from "./_components/Pricing";
 import { Testimonials } from "./_components/Testimonials";
@@ -6,6 +7,7 @@ export default async function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-white text-white dark:border-b-slate-700 dark:bg-background">
       <Hero />
+      <Features />
       <Testimonials />
       <Pricing />
     </main>
