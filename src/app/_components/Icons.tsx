718b3292a60e3ab20da4202e0c78ef3be0b9a092
Malith Kuruppu <info.mkappworks@gmail.com>
2024-05-08 16:09:35 +0200

    Added dark logo icon

--- src/app/_components/Icons.tsx
+++ src/app/_components/Icons.tsx
@@ -1,7 +1,12 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import Image from "next/image";
 import logoIcon from "../../../public/logo.svg";
+import logoIconDark from "../../../public/logo_dark.svg";
 
 export const LogoIcon = () => {
   return <Image priority src={logoIcon} alt="Photo Album" width={42} />;
+};
+
+export const LogoIconDark = () => {
+  return <Image priority src={logoIconDark} alt="Photo Album" width={42} />;
 };
--- src/app/_components/layout/Navbar.tsx
+++ src/app/_components/layout/Navbar.tsx
@@ -17,7 +17,8 @@ import {
 import { buttonVariants } from "../ui/button";
 import { Menu } from "lucide-react";
 import { ModeToggle } from "../ModeToggle";
-import { LogoIcon } from "../Icons";
+import { LogoIcon, LogoIconDark } from "../Icons";
+import { useTheme } from "next-themes";
 
 interface RouteProps {
   href: string;
@@ -45,8 +46,10 @@ const routeList: RouteProps[] = [
 
 export const Navbar = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
+  const { theme } = useTheme();
+
   return (
-    <header className="dark:bg-background sticky top-0 z-40 w-full border-b-[1px] bg-white dark:border-b-slate-700">
+    <header className="sticky top-0 z-40 w-full border-b-[1px] bg-white dark:border-b-slate-700 dark:bg-background">
       <NavigationMenu className="mx-auto">
         <NavigationMenuList className="container flex h-14 w-screen justify-between px-4 ">
           <NavigationMenuItem className="flex font-bold">
@@ -55,7 +58,7 @@ export const Navbar = () => {
               href="/"
               className="ml-2 flex  items-center justify-center text-xl font-bold"
             >
-              <LogoIcon />
+              {theme !== "light" ? <LogoIconDark /> : <LogoIcon />}
               <span className="ml-2">Photo Album</span>
             </a>
           </NavigationMenuItem>
