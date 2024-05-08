3236f951a6599c9d9e60aa563fb410c43330137e
Malith Kuruppu <info.mkappworks@gmail.com>
2024-05-08 23:43:17 +0200

    Added Signin functionality

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,10 @@ import "~/styles/globals.css";
 import { Inter } from "next/font/google";
 
 import { TRPCReactProvider } from "~/trpc/react";
-import { Navbar } from "~/app/_components/layout/Navbar";
 import { ThemeProvider } from "./_components/ThemeProvider";
 import { settings } from "~/lib/settings";
 import { Footer } from "./_components/layout/Footer";
+import { Navbar } from "./_components/layout/NavBar";
 
 const inter = Inter({
   subsets: ["latin"],
