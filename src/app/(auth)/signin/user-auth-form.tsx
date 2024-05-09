31db19c89a736cc789c38fbcd854958090e30a23
Malith Kuruppu <info.mkappworks@gmail.com>
2024-05-09 16:36:08 +0200

    Added UserFormValue

---
+++ src/app/(auth)/signin/user-auth-form.tsx
@@ -0,0 +1,139 @@
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+"use client";
+
+import { useSearchParams } from "next/navigation";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { signIn } from "next-auth/react";
+import { GoogleIcon } from "~/app/_components/home/icons";
+import { Button } from "~/app/_components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "../../_components/ui/form";
+import { Input } from "../../_components/ui/input";
+
+export const signInSchema = z.object({
+  email: z
+    .string({ required_error: "Email is required" })
+    .min(1, "Email is required")
+    .email("Invalid email"),
+  password: z
+    .string({ required_error: "Password is required" })
+    .min(1, "Password is required")
+    .min(8, "Password must be more than 8 characters")
+    .max(32, "Password must be less than 32 characters"),
+});
+
+type UserFormValue = z.infer<typeof signInSchema>;
+
+export const UserAuthForm = () => {
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl");
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const form = useForm<UserFormValue>({
+    resolver: zodResolver(signInSchema),
+  });
+
+  const onSubmit = async (data: UserFormValue) => {
+    setLoading(true);
+    await signIn("credentials", {
+      email: data.email,
+      callbackUrl: callbackUrl ?? "/",
+    });
+
+    setLoading(false);
+  };
+
+  return (
+    <>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="w-full space-y-2"
+        >
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input
+                    type="email"
+                    placeholder="Enter your email..."
+                    disabled={loading}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="Enter your password..."
+                    disabled={loading}
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+
+          <Button disabled={loading} className="ml-auto w-full" type="submit">
+            Continue With Email
+          </Button>
+        </form>
+      </Form>
+      <div className="relative">
+        <div className="absolute inset-0 flex items-center">
+          <span className="w-full border-t" />
+        </div>
+        <div className="relative flex justify-center text-xs uppercase">
+          <span className="bg-background px-2 text-muted-foreground">
+            Or continue with
+          </span>
+        </div>
+      </div>
+      <GoogleSignInButton isDisabled={loading} />
+    </>
+  );
+};
+
+const GoogleSignInButton = ({ isDisabled }: { isDisabled: boolean }) => {
+  const searchParams = useSearchParams();
+  const callbackUrl = searchParams.get("callbackUrl");
+
+  return (
+    <Button
+      className="w-full"
+      variant="outline"
+      type="button"
+      disabled={isDisabled}
+      onClick={() => signIn("google", { callbackUrl: callbackUrl ?? "/" })}
+    >
+      <GoogleIcon />
+      <span className="ml-2">Continue with Google</span>
+    </Button>
+  );
+};
