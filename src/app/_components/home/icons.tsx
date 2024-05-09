f5ee42b07c0a8059a239323757c0ea287c21e841
Malith Kuruppu <info.mkappworks@gmail.com>
2024-05-09 16:35:25 +0200

    Added GoogleIcon

--- src/app/_components/home/icons.tsx
+++ src/app/_components/home/icons.tsx
@@ -1,8 +1,19 @@
 import Image, { type StaticImageData } from "next/image";
 import logoIcon from "../../../../public/logo.svg";
+import googleIcon from "../../../../public/google.svg";
 
 export const LogoIcon = () => {
   return (
-    <Image src={logoIcon as StaticImageData} alt="Photo Album" width={42} />
+    <Image
+      src={logoIcon as StaticImageData}
+      alt="Photo Album Logo"
+      width={42}
+    />
+  );
+};
+
+export const GoogleIcon = () => {
+  return (
+    <Image src={googleIcon as StaticImageData} alt="Google Icon" width={24} />
   );
 };
