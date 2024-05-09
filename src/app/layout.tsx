2415a8b806d2b3a0b7e25dce1d38edb72903820a
Malith Kuruppu <info.mkappworks@gmail.com>
2024-05-09 12:35:15 +0200

    Updated layout to use Providers and get auth session

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,10 +3,11 @@ import "~/styles/globals.css";
 import { Inter } from "next/font/google";
 
 import { TRPCReactProvider } from "~/trpc/react";
-import { ThemeProvider } from "./_components/ThemeProvider";
-import { settings } from "~/lib/settings";
+
+import { getServerAuthSession } from "~/server/auth";
+import { Providers } from "./_components/Providers";
+import { Navbar } from "./_components/layout/Navbar";
 import { Footer } from "./_components/layout/Footer";
-import { Navbar } from "./_components/layout/NavBar";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -26,30 +27,24 @@ export const viewport = {
   ],
 };
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerAuthSession();
+
   return (
     <html lang="en">
       <body
         className={`${inter.className} flex min-h-screen flex-col bg-background text-primary`}
       >
         <TRPCReactProvider>
-          {settings.themeToggleEnabled ? (
-            <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
-              <Navbar />
-              {children}
-              <Footer />
-            </ThemeProvider>
-          ) : (
-            <ThemeProvider attribute="class" forcedTheme="light" enableSystem>
-              <Navbar />
-              {children}
-              <Footer />
-            </ThemeProvider>
-          )}
+          <Providers session={session}>
+            <Navbar />
+            {children}
+            <Footer />
+          </Providers>
         </TRPCReactProvider>
       </body>
     </html>
