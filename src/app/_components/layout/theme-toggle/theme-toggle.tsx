8344dd78e1e506fc5b545946b83ff45c7f7b44db
Malith Kuruppu <info.mkappworks@gmail.com>
2024-05-09 13:14:34 +0200

    Updated ThemeToggle style

--- src/app/_components/layout/theme-toggle/theme-toggle.tsx
+++ src/app/_components/layout/theme-toggle/theme-toggle.tsx
@@ -1,3 +1,8 @@
+"use client";
+
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import { useTheme } from "next-themes";
+
 import { Button } from "../../ui/button";
 import {
   DropdownMenu,
@@ -5,18 +10,16 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "../../ui/dropdown-menu";
-import { useTheme } from "next-themes";
-import { Moon, Sun } from "lucide-react";
 
-export function ThemeToggle() {
+export const ThemeToggle = () => {
   const { setTheme } = useTheme();
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="ghost" size="icon" className="ghost">
-          <Sun className="h-[1.1rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.1rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <Button variant="outline" size="icon">
+          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
@@ -33,4 +36,4 @@ export function ThemeToggle() {
       </DropdownMenuContent>
     </DropdownMenu>
   );
-}
+};
