6a9845f8afa7414ff4930f86bf8f4a80aefd4870
Malith Kuruppu <info.mkappworks@gmail.com>
2024-05-09 16:36:20 +0200

    Added Signin page and layout

---
+++ src/app/(auth)/signin/page.tsx
@@ -0,0 +1,50 @@
+import { type Metadata } from "next";
+import Link from "next/link";
+
+import { UserAuthForm } from "./user-auth-form";
+import { LogoIconName } from "~/app/_components/logo-icon-name";
+
+export const metadata: Metadata = {
+  title: "Photo Album - SignIn",
+  description: "Sign in to your account",
+};
+
+export default function SignInPage() {
+  return (
+    <div className="relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <div className="relative hidden h-full flex-col bg-muted p-10 text-white dark:border-r lg:flex">
+        <LogoIconName />
+      </div>
+      <div className="flex h-full items-center p-4 lg:p-8">
+        <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
+          <div className="flex flex-col space-y-2 text-center">
+            <h1 className="text-2xl font-semibold tracking-tight">
+              Create an account
+            </h1>
+            <p className="text-sm text-muted-foreground">
+              Enter your email below to create your account
+            </p>
+          </div>
+          <UserAuthForm />
+          <p className="px-8 text-center text-sm text-muted-foreground">
+            By clicking continue, you agree to our{" "}
+            <Link
+              href="/terms"
+              className="underline underline-offset-4 hover:text-primary"
+            >
+              Terms of Service
+            </Link>{" "}
+            and{" "}
+            <Link
+              href="/privacy"
+              className="underline underline-offset-4 hover:text-primary"
+            >
+              Privacy Policy
+            </Link>
+            .
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
