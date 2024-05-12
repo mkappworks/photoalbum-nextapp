32c9d59c769412fa0a3b6ee0403e60a039a1bddf
Malith Kuruppu <info.mkappworks@gmail.com>
2024-05-12 18:04:43 +0200

    Added user register

---
+++ src/server/api/routers/user.ts
@@ -0,0 +1,21 @@
+import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+import { users } from "@/server/db/schema";
+import { CredentialsSchema } from "@/schema/credentials";
+import bcrypt from "bcrypt";
+import { v4 as uuidv4 } from "uuid";
+
+export const userRouter = createTRPCRouter({
+  register: publicProcedure
+    .input(CredentialsSchema)
+    .mutation(async ({ ctx, input }) => {
+      const saltRounds = 10;
+      const salt = await bcrypt.genSalt(saltRounds);
+      const hash = await bcrypt.hash(input.password, salt);
+
+      await ctx.db.insert(users).values({
+        id: uuidv4(),
+        email: input.email,
+        password: hash,
+      });
+    }),
+});
