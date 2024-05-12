6f1a70b3c0b83c48776bc75e422a947106564a1f
Malith Kuruppu <info.mkappworks@gmail.com>
2024-05-12 18:02:52 +0200

    Updated user schema with password

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -17,7 +17,9 @@ import { type AdapterAccount } from "next-auth/adapters";
  *
  * @see https://orm.drizzle.team/docs/goodies#multi-project-schema
  */
-export const createTable = pgTableCreator((name) => `photoalbum-nextapp_${name}`);
+export const createTable = pgTableCreator(
+  (name) => `photoalbum-nextapp_${name}`,
+);
 
 export const posts = createTable(
   "post",
@@ -35,7 +37,7 @@ export const posts = createTable(
   (example) => ({
     createdByIdIdx: index("createdById_idx").on(example.createdById),
     nameIndex: index("name_idx").on(example.name),
-  })
+  }),
 );
 
 export const users = createTable("user", {
@@ -45,6 +47,7 @@ export const users = createTable("user", {
   emailVerified: timestamp("emailVerified", {
     mode: "date",
   }).default(sql`CURRENT_TIMESTAMP`),
+  password: text("password"),
   image: varchar("image", { length: 255 }),
 });
 
@@ -76,7 +79,7 @@ export const accounts = createTable(
       columns: [account.provider, account.providerAccountId],
     }),
     userIdIdx: index("account_userId_idx").on(account.userId),
-  })
+  }),
 );
 
 export const accountsRelations = relations(accounts, ({ one }) => ({
@@ -96,7 +99,7 @@ export const sessions = createTable(
   },
   (session) => ({
     userIdIdx: index("session_userId_idx").on(session.userId),
-  })
+  }),
 );
 
 export const sessionsRelations = relations(sessions, ({ one }) => ({
@@ -112,5 +115,5 @@ export const verificationTokens = createTable(
   },
   (vt) => ({
     compoundKey: primaryKey({ columns: [vt.identifier, vt.token] }),
-  })
+  }),
 );
