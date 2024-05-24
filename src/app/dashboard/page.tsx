45265d60b8d7445ef51c8cd67c892684c9862971
Malith Kuruppu <info.mkappworks@gmail.com>
2024-05-24 15:41:52 +0200

    Added DashboardPage

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default async function DashboardPage() {
+  return (
+    <div className="relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
+      <h1>Dashboard</h1>
+    </div>
+  );
+}
