3044c6fd9a979e68a262cb6df44fc1642568deb3
Malith Kuruppu <info.mkappworks@gmail.com>
2024-05-24 18:33:21 +0200

    Added createMagicLinkUser

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,21 +1,34 @@
-import { CredentialsSchema } from "@/schema/credentials";
+import { MagicLinkSchema } from "@/schema/magic-link";
 import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
 import { users } from "@/server/db/schema";
-import bcrypt from "bcrypt";
 import { v4 as uuidv4 } from "uuid";
 
 export const userRouter = createTRPCRouter({
   register: publicProcedure
-    .input(CredentialsSchema)
+    .input(MagicLinkSchema)
     .mutation(async ({ ctx, input }) => {
-      const saltRounds = 10;
-      const salt = await bcrypt.genSalt(saltRounds);
-      const hash = await bcrypt.hash(input.password, salt);
-
       await ctx.db.insert(users).values({
         id: uuidv4(),
         email: input.email,
-        password: hash,
       });
+    }),
+  createMagicLinkUser: publicProcedure
+    .input(MagicLinkSchema)
+    .mutation(async ({ ctx, input }) => {
+      const existingUser = await ctx.db.query.users.findFirst({
+        where: (users, { eq }) => eq(users.email, input.email),
+      });
+
+      let updatedUser;
+
+      if (!existingUser) {
+        updatedUser = await ctx.db.insert(users).values({
+          id: uuidv4(),
+          email: input.email,
+        });
+      } else {
+      }
+
+      return updatedUser;
     }),
 });
